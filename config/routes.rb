428cff9a72c58c3d9ce5f7185740fdaac5be980b
森瀬 隆司 <55taac.tac@gmail.com>
2019-01-08 19:44:28 +0900

    Section 1, 2, 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :books
+  get 'hello/index',to: 'hello#index'
+  get 'hello/view' => 'hello#view'
+  get 'hello/list', to: 'hello#list'
 end
